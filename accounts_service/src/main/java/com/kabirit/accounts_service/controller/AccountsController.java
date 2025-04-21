4f81edbd1d13a3d9cbfdd800e8c0b0e221785e44
Nasim Kabir <mdkabir334@gmail.com>
2025-04-21 21:48:42 +0600

    spring boot profile management impl done

--- accounts_service/src/main/java/com/kabirit/accounts_service/controller/AccountsController.java
+++ accounts_service/src/main/java/com/kabirit/accounts_service/controller/AccountsController.java
@@ -1,11 +1,17 @@
 package com.kabirit.accounts_service.controller;
 
+import com.kabirit.accounts_service.dto.AccountsContactInfoDto;
 import com.kabirit.accounts_service.dto.CustomerDto;
 import com.kabirit.accounts_service.exceptions.Response;
+import com.kabirit.accounts_service.exceptions.ResponseBuilder;
 import com.kabirit.accounts_service.service.IAccountsService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +22,17 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Validated
 public class AccountsController {
 
     private final IAccountsService accountsService;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    private final Environment environment;
+    private final AccountsContactInfoDto accountsContactInfoDto;
 
     @PostMapping("/create")
     public Response createAccount(@Valid @RequestBody CustomerDto customerDto) {
@@ -47,6 +59,23 @@ public class AccountsController {
                                          @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
                                          String mobileNumber) {
         return accountsService.deleteAccount(mobileNumber);
+    }
+
+
+    @GetMapping("/build-info")
+    public Response getBuildInfo() {
+        return ResponseBuilder.getSuccessResponse(HttpStatus.OK, "Build Version: " + buildVersion);
+    }
+
+    @GetMapping("/java-version")
+    public Response getJavaVersion() {
+        return ResponseBuilder.getSuccessResponse(HttpStatus.OK, "JDK Version: " + environment.getProperty("JAVA_HOME"));
+    }
+
+
+    @GetMapping("/contact-info")
+    public Response getContactInfo() {
+        return ResponseBuilder.getSuccessResponse(HttpStatus.OK, "Accounts Contact Info ", accountsContactInfoDto);
     }
 
 
